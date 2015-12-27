4c1c1767ac1624e6f2c640c6d8e4fe3acd260990
Henry Gamba <henrypgamba@gmail.com>
2015-12-27 01:43:50 -0200

    First
    Point n click move

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Debug.Log(ray.origin.x + " " + ray.origin.y);
+            Vector2 new_pos = new Vector2(ray.origin.x, ray.origin.y);
+            transform.position = new_pos;
+
+        }
+
+    }
+}
