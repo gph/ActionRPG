66490cf1761208edb4de98d5903027f4320a90f5
Henry Gamba <henrypgamba@gmail.com>
2015-12-29 21:28:26 -0200

    spell cast aura
    trash

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,17 +8,19 @@ public class PlayerController : MonoBehaviour{
     private Vector3 targetPosition, velocity = Vector3.zero;
     public float maxSpeed = 5f;
     public Camera mainCamera;
-    public float jumpHeight = 8f;
 
-    public Transform targetClicked;
+    //public Transform targetClicked;
     public GameObject iceSpike;
 
-    private bool magicRunning = false;
+    private bool magicActivated = false;
     private bool spellCasted = false;
+
+    Animator playerAnimator;
 
     // Use this for initialization
     void Start () {
         targetPosition = transform.position;
+        playerAnimator = GetComponent<Animator>();
     }
 	
 	// Update is called once per frame
@@ -26,8 +28,10 @@ public class PlayerController : MonoBehaviour{
         // camera follows player position
         mainCamera.transform.position = new Vector3(transform.position.x, mainCamera.transform.position.y, mainCamera.transform.position.z);
 
+        // convert mouse position to screen pos
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        if (!magicRunning)
+
+        if (!magicActivated)
         {
             targetPosition = transform.position;
             if (Input.GetAxisRaw("Fire1") != 0)
@@ -58,27 +62,24 @@ public class PlayerController : MonoBehaviour{
                 
                 if (ray.origin.y > -1)
                 {
-                    targetClicked.position = new Vector3(ray.origin.x, transform.position.y, transform.position.z);
+                    targetPosition = new Vector3(ray.origin.x, transform.position.y, transform.position.z);
                 }
+
                 // instatiate iceSpike from prefab
                 //iceSpike = (GameObject)Instantiate(iceSpike, new Vector3(targetClicked.position.x, 5, targetClicked.position.z),transform.rotation);
+
                 if (!spellCasted)
                 {
-                    StartCoroutine(SpellCast(0.5F, targetClicked.position));
+                    StartCoroutine(SpellCast(0.5F, targetPosition));
                 }                
                 spellCasted = true;
-
+                playerAnimator.SetBool("casting", true);
             }
         }
     }    
     public void magic()
     {
-        magicRunning = !magicRunning;
-    }
-
-    void OnMouseDown()
-    {
-        // this object was clicked - do something
+        magicActivated = !magicActivated;
     }
     IEnumerator SpellCast(float waitTime, Vector3 position)
     {
@@ -86,7 +87,8 @@ public class PlayerController : MonoBehaviour{
         yield return new WaitForSeconds(waitTime);
         iceSpike.transform.position = new Vector3(position.x, 5, position.z);
         iceSpike.gameObject.SetActive(true);
-        magicRunning = !magicRunning;
+        magicActivated = !magicActivated;
         spellCasted = false;
+        playerAnimator.SetBool("casting", false);
     }
 }
